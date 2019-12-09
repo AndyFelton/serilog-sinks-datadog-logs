2a05a33c9190f1ca8a6dbe3fc261e3f4cfbb7131
Andy Felton <andy.felton@nemisys.uk.com>
2019-12-09 15:04:40 +0000

    Code for testing

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Serilog.Sinks.Datadog.Logs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Serilog.Debugging.SelfLog.Enable(Console.Error);
+
+            var config = new DatadogConfiguration(url: "tcp-intake.logs.datadoghq.eu", port: 443, useSSL: true, useTCP: true);
+            var log = new LoggerConfiguration()
+                .WriteTo.DatadogLogs(
+                    "407f0d06e5c7057d58321048e24dae7d",
+                    source: "source",
+                    service: "service",
+                    host: "hostname",
+                    configuration: config
+                )
+                .CreateLogger();
+
+            while (true)
+            {
+                log.Warning("Disk quota {Quota} MB exceeded by {User}", 111, 222);
+                System.Threading.Thread.Sleep(60000);
+            }
+
+        }
+    }
+}
